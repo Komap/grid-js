9b81461e066f478f93e2951e8230bb574c5fea7e
igogo5yo <skliar.ihor@gmail.com>
2017-04-13 16:49:26 +0300

    added .gitignore and data.js

---
+++ js/data.js
@@ -0,0 +1,60 @@
+var columns = [
+    {
+        key: 'id',
+        title: 'ID'
+    },
+    {
+        key: 'firstName',
+        title: 'First Name'
+    },
+    {
+        key: 'lastName',
+        title: 'Lastname Name'
+    },
+    {
+        key: 'birthday',
+        title: 'Birth date'
+    },
+    {
+        key: 'salary',
+        title: 'Salary'
+    }
+];
+
+var data = [
+    {
+        id: '1',
+        firstName: 'Vasya',
+        lastName: 'Petrov',
+        birthday: '1999-05-10',
+        salary: 3000
+    },
+    {
+        id: '2',
+        firstName: 'Petya',
+        lastName: 'Valiliev',
+        birthday: '1995-02-22',
+        salary: 4000
+    },
+    {
+        id: '3',
+        firstName: 'Serega',
+        lastName: 'Grigiriev',
+        birthday: '1996-02-14',
+        salary: 5000
+    },
+    {
+        id: '4',
+        firstName: 'Max',
+        lastName: 'Petrovski',
+        birthday: '1997-03-02',
+        salary: 6000
+    },
+    {
+        id: '5',
+        firstName: 'Koliya',
+        lastName: 'Ivanov',
+        birthday: '1998-04-11',
+        salary: 7000
+    }
+];
